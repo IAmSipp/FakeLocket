1406024584f2e06f9bed4a632f87d5e2e586eb46
Sippx <amjustafly@gmail.com>
2024-09-08 17:15:03 +0700

    Change Name

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $friends_result = $stmt->get_result();
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#"><b>BFriend</b></a>
+            <a class="navbar-brand" href="#"><b>Nugget</b></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
